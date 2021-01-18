a858662822d27d716d2206208e2821bb5aeea608
Tech-Expert-Wizard <77369194+Tech-Expert-Wizard@users.noreply.github.com>
2021-01-18 10:22:26 -0500

    Making the code look better

--- BOT.py
+++ BOT.py
@@ -10,7 +10,8 @@ room = se_chat.get_room(os.environ['ROOM'])
 def on_event(event, _):
     if event.data['event_type'] == 1:
         msg = event.message.content_source
-        # Make a bot similar to Smokey      
+        # Make a bot similar to Smokey
+
 room.join()
 room.watch_socket(on_event)
 while True:
