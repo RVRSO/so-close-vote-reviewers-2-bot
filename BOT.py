ddebc7d09b16b8b3902219658997ed5cd530fee2
Tech-Expert-Wizard <77369194+Tech-Expert-Wizard@users.noreply.github.com>
2021-01-18 08:01:04 -0500

    Adding basic chatbot code framework

--- BOT.py
+++ BOT.py
@@ -4,6 +4,12 @@ password = '**************'
 se_chat = Client('stackoverflow.com')
 se_chat.login(email, password)
 room = se_chat.get_room('227446')
+def on_event(event, _):
+  if event.data['event_type'] == 1:
+    msg = event.message.content_source
+    #Make a bot similar to Smokey
 room.join()
+room.watch_socket(on_event)
 room.send_message('@ppw Are you here?! lol ;D ;)')
-room.leave()
+while True:
+  pass
