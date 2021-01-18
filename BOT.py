58e0566daa6c2b3ac4ba55bbbdb2d96394dd0a09
Victor-VosMottor <72744550+Victor-VosMottor@users.noreply.github.com>
2021-01-18 18:19:25 +0300

    Made the code look better and made it more secure
    Made the code look better and made it more secure

--- BOT.py
+++ BOT.py
@@ -1,14 +1,19 @@
 from chatexchange.client import Client
-email = 'email@email.com'
-password = '**************'
-se_chat = Client('stackoverflow.com')
+import os
+
+email = os.environ['EMAIL']
+password = os.environ['PASSWORD']
+se_chat = Client(os.environ['CLIENT'])
 se_chat.login(email, password)
-room = se_chat.get_room('227446')
+room = se_chat.get_room(os.environ['ROOM'])
+
 def on_event(event, _):
-  if event.data['event_type'] == 1:
-    msg = event.message.content_source
-    #Make a bot similar to Smokey
+    if event.data['event_type']:
+        msg = event.message.content_source
+        # Make a bot similar to Smokey
+        
 room.join()
 room.watch_socket(on_event)
+
 while True:
-  pass
+    pass
