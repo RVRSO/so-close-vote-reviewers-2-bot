3cb2280b35176c9835625e60a8da76e780e5fd29
ppwater01 <69420216+ppwater01@users.noreply.github.com>
2021-01-24 01:56:39 +0000

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -1,18 +1,19 @@
 from chatexchange.client import Client
-from os import environ
+import os
 
-email = environ['EMAIL']
-password = environ['PASSWORD']
-se_chat = Client('stackoverflow.com')
+email = os.environ['EMAIL']
+password = os.environ['PASSWORD']
+se_chat = Client(os.environ['CLIENT'])
 se_chat.login(email, password)
-room = se_chat.get_room('227446')
+room = se_chat.get_room(os.environ['ROOM'])
 
 def on_event(event, _):
-    if event.data['event_type'] == 1:
+    if event.data['event_type']:
         msg = event.message.content_source
         # Make a bot similar to Smokey
-
+        
 room.join()
 room.watch_socket(on_event)
+
 while True:
     pass
